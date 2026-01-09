da6b2263fd00da8036cdf257f101c5063ffe3fce
israelzw <168639914+israelzw@users.noreply.github.com>
2026-01-09 13:05:52 +0200

    Add files via upload

---
+++ dataAgency.js
@@ -0,0 +1,230 @@
+const stopsDataBoos = [
+  {
+    "agency_id": "2",
+    "agency_name": "רכבת ישראל",
+    "agency_url": "http://www.rail.co.il",
+    "agency_phone": "*5770"
+  },
+  {
+    "agency_id": "3",
+    "agency_name": "אגד",
+    "agency_url": "http://www.egged.co.il",
+    "agency_phone": "*2800"
+  },
+  {
+    "agency_id": "4",
+    "agency_name": "אלקטרה אפיקים תחבורה",
+    "agency_url": "http://www.egged-taavura.co.il",
+    "agency_phone": "*6686"
+  },
+  {
+    "agency_id": "5",
+    "agency_name": "דן",
+    "agency_url": "http://www.dan.co.il",
+    "agency_phone": "*3456"
+  },
+  {
+    "agency_id": "6",
+    "agency_name": "ש.א.מ",
+    "agency_url": "http://www.nazareth-unbs.com",
+    "agency_phone": "*3553"
+  },
+  {
+    "agency_id": "7",
+    "agency_name": "נסיעות ותיירות",
+    "agency_url": "http://www.ntt-buses.com",
+    "agency_phone": "*3553"
+  },
+  {
+    "agency_id": "8",
+    "agency_name": "גי.בי.טורס",
+    "agency_url": "http://www.gb-tours.com",
+    "agency_phone": "046036100"
+  },
+  {
+    "agency_id": "10",
+    "agency_name": "מועצה אזורית אילות",
+    "agency_url": "https://www.eilot.org.il/bus/",
+    "agency_phone": "054-7880000"
+  },
+  {
+    "agency_id": "14",
+    "agency_name": "נתיב אקספרס",
+    "agency_url": "http://www.nateevexpress.com",
+    "agency_phone": "*3553"
+  },
+  {
+    "agency_id": "15",
+    "agency_name": "מטרופולין",
+    "agency_url": "http://www.metropoline.com",
+    "agency_phone": "*5900"
+  },
+  {
+    "agency_id": "16",
+    "agency_name": "סופרבוס",
+    "agency_url": "http://www.superbus.co.il",
+    "agency_phone": "1-700-700-181"
+  },
+  {
+    "agency_id": "18",
+    "agency_name": "קווים",
+    "agency_url": "http://www.kavim-t.co.il",
+    "agency_phone": "*2060"
+  },
+  {
+    "agency_id": "20",
+    "agency_name": "כרמלית",
+    "agency_url": "http://www.carmelithaifa.co.il",
+    "agency_phone": "*8787"
+  },
+  {
+    "agency_id": "21",
+    "agency_name": "כפיר",
+    "agency_url": "https://www.cfir.co.il/",
+    "agency_phone": "*8787"
+  },
+  {
+    "agency_id": "22",
+    "agency_name": "תבל",
+    "agency_url": "http://www.callkav.gov.il/",
+    "agency_phone": "*9943"
+  },
+  {
+    "agency_id": "23",
+    "agency_name": "גלים",
+    "agency_url": "http://www.galeem.co.il/",
+    "agency_phone": "0737968430"
+  },
+  {
+    "agency_id": "24",
+    "agency_name": "מועצה אזורית גולן",
+    "agency_url": "https://www.golan.org.il/",
+    "agency_phone": "*3254"
+  },
+  {
+    "agency_id": "25",
+    "agency_name": "אלקטרה אפיקים",
+    "agency_url": "https://www.electra-afikim.co.il/",
+    "agency_phone": "*6686"
+  },
+  {
+    "agency_id": "31",
+    "agency_name": "דן בדרום",
+    "agency_url": "http://www.danbadarom.co.il/",
+    "agency_phone": "*5467"
+  },
+  {
+    "agency_id": "32",
+    "agency_name": "דן באר שבע",
+    "agency_url": "http://www.danbr7.co.il/",
+    "agency_phone": "*3527"
+  },
+  {
+    "agency_id": "33",
+    "agency_name": "כבל אקספרס",
+    "agency_url": "https://cableexpress.co",
+    "agency_phone": "*8787"
+  },
+  {
+    "agency_id": "34",
+    "agency_name": "תנופה",
+    "agency_url": "https://www.tnufa-t.com/",
+    "agency_phone": "*8797"
+  },
+  {
+    "agency_id": "35",
+    "agency_name": "בית שמש אקספרס",
+    "agency_url": "http://www.bs-exp.co.il/",
+    "agency_phone": "*3343"
+  },
+  {
+    "agency_id": "37",
+    "agency_name": "אקסטרה",
+    "agency_url": "https://extrapt.co.il/",
+    "agency_phone": "*6747"
+  },
+  {
+    "agency_id": "38",
+    "agency_name": "אקסטרה ירושלים",
+    "agency_url": "https://extrapt.co.il/",
+    "agency_phone": "*6747"
+  },
+  {
+    "agency_id": "40",
+    "agency_name": "יונייטד טורס",
+    "agency_url": "https://www.unitedtours.co.il",
+    "agency_phone": "*8787"
+  },
+  {
+    "agency_id": "42",
+    "agency_name": "ירושלים-רמאללה איחוד",
+    "agency_url": "http://www.ramallahbus.com/ar",
+    "agency_phone": "02-6274334"
+  },
+  {
+    "agency_id": "44",
+    "agency_name": "ירושלים-אבו-תור-ענאתא איחוד",
+    "agency_url": "http://www.atabuses.com/ar",
+    "agency_phone": "02-6277446"
+  },
+  {
+    "agency_id": "45",
+    "agency_name": "ירושלים-אלווסט איחוד",
+    "agency_url": "http://www.centerbuses.com/ar",
+    "agency_phone": "02-5725222"
+  },
+  {
+    "agency_id": "47",
+    "agency_name": "ירושלים-הר הזיתים",
+    "agency_url": "http://mountofolivesbus.com/?lang=ar",
+    "agency_phone": "02-6285551"
+  },
+  {
+    "agency_id": "49",
+    "agency_name": "ירושלים - עיסאוויה מחנה שעפאט איחוד",
+    "agency_url": "http://www.shufatbus.com",
+    "agency_phone": "02-5827547"
+  },
+  {
+    "agency_id": "50",
+    "agency_name": "ירושלים-דרום איחוד",
+    "agency_url": "http://www.south-buses.com",
+    "agency_phone": "02-5798587"
+  },
+  {
+    "agency_id": "51",
+    "agency_name": "ירושלים-צור באהר איחוד",
+    "agency_url": "http://www.surbaherbus.com",
+    "agency_phone": "02-6735130"
+  },
+  {
+    "agency_id": "91",
+    "agency_name": "מוניות מטרו קו",
+    "agency_url": "http://www.metrokav.com",
+    "agency_phone": "0737801500"
+  },
+  {
+    "agency_id": "93",
+    "agency_name": "מוניות מאיה יצחק שדה",
+    "agency_url": "http://www.bus.gov.il",
+    "agency_phone": "089211110"
+  },
+  {
+    "agency_id": "97",
+    "agency_name": "אודליה מוניות בעמ",
+    "agency_url": "http://www.bus.gov.il",
+    "agency_phone": "*8787"
+  },
+  {
+    "agency_id": "98",
+    "agency_name": "מוניות רב קווית 4-5",
+    "agency_url": "https://isrssl.isrcorp.co.il:8093/taxi_45",
+    "agency_phone": "*8787"
+  },
+  {
+    "agency_id": "135",
+    "agency_name": "דרך אגד עוטף ירושלים",
+    "agency_url": "http://www.egged.co.il",
+    "agency_phone": "*8787"
+  }
+]
